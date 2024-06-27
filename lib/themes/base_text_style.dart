eb1ce5393e37c4e5269d523fb089025bb66cabc2
Bagus Merta <merta.bagus24@gmail.com>
2024-06-27 23:05:59 +0800

    feat: add base text style

---
+++ lib/themes/base_text_style.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:palmkindle/utils/font_family.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// Display, Headline, Title, Body, Label
+// Type. Large, Medium, Small
+extension BaseTextStyle on TextStyle {
+  static TextStyle displayLarge = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 57.sp,
+    height: 64.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+
+  static TextStyle displayMedium = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 45.sp,
+    height: 52.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+
+  static TextStyle displaySmall = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 36.sp,
+    height: 44.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+
+  static TextStyle headlineLarge = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 32.sp,
+    height: 40.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+
+  static TextStyle headlineMedium = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 28.sp,
+    height: 36.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+
+  static TextStyle headlineSmall = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 24.sp,
+    height: 32.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+
+  static TextStyle titleLarge = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 22.sp,
+    height: 28.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
+  static TextStyle titleMedium = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 16.sp,
+    height: 24.sp,
+    letterSpacing: 0.15.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
+  static TextStyle titleSmall = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 14.sp,
+    height: 20.sp,
+    letterSpacing: 0.1.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyLarge = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 16.sp,
+    height: 24.sp,
+    letterSpacing: 0.15.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+
+  static TextStyle bodyMedium = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 14.sp,
+    height: 20.sp,
+    letterSpacing: 0.25.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+
+  static TextStyle bodySmall = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 12.sp,
+    height: 20.sp,
+    letterSpacing: 0.4.sp,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+
+  static TextStyle labelLarge = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 14.sp,
+    height: 20.sp,
+    letterSpacing: 0.1.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
+  static TextStyle labelMedium = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 12.sp,
+    height: 16.sp,
+    letterSpacing: 0.5.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
+  static TextStyle labelSmall = TextStyle(
+    fontFamily: FontFamily.golos,
+    fontSize: 11.sp,
+    height: 16.sp,
+    letterSpacing: 0.1.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+}
---
+++ lib/utils/font_family.dart
@@ -0,0 +1,8 @@
+// * FontFamily Class
+// Using font name string directly is not safe.
+// What would happen if you made a typo
+class FontFamily {
+  FontFamily._();
+
+  static const String golos = 'Golos';
+}
